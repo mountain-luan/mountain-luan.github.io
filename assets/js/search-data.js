cbd8adc0cd7a9bd4749d11db3ea4f67f8f80bc27
mountain-luan <yyluan1999@gmail.com>
2025-12-18 16:48:38 +0000

    Deploying to gh-pages from @ mountain-luan/mountain-luan.github.io@ec49168581f6c61de0acde28fd83985599c15ad3 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -398,11 +398,6 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
-          description: "",
-          section: "News",handler: () => {
-              window.location.href = "/news/announcement_2/";
             },},{id: "news-my-personal-website-is-online",
           title: 'My personal website is online.',
           description: "",
